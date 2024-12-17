6037a0a279918f91d601a2f3ec783b00538cafe9
Lisa BELLALI <lisa.bellali92@gmail.com>
2024-12-17 12:32:58 +0100

    index series

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,13 @@
+
+
+const indexSeries = [
+  {title: "Yellowstone", année: "2018", nbsaisons: "5"},
+  {title: "Dune : Prophecy", année: "2024", nbsaisons: "1"},
+  {title: "Arcane", année: "2021", nbsaisons: "1"},
+  {title: "Secret Level", année: "2024", nbsaisons: "1"},
+  {title: "Black Doves", année: "2024", nbsaisons: "En cours"}
+  ]
+
 function App() {
   return (
     <>
