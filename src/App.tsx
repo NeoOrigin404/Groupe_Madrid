2149377a00b9985e8dc646a4e781b05578bdbc51
Julien PICART <julienpicart01@gmail.com>
2024-12-17 11:52:48 +0100

    Ajout des composents FilmList et FilmCard - non fonctionnel

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import FilmList from "./components/FilmList";
+import FilmCard from "./components/FilmCard";
+
 export default function App() {
   return (
     <>
       <h1>TV Time</h1>
+      <FilmCard />
     </>
   );
 }
---
+++ src/components/FilmCard.tsx
@@ -0,0 +1,44 @@
+const filmsSelection = [
+  {
+    affiche:
+      "https://www.tvtime.com/_next/image?url=https%3A%2F%2Fartworks.thetvdb.com%2Fbanners%2Fv4%2Fmovie%2F340965%2Fposters%2F674170a914b85.jpg&w=640&q=75",
+    titre: "Carry-On",
+    duree: "1h59",
+    annee: "2024",
+  },
+  {
+    affiche:
+      "https://www.tvtime.com/_next/image?url=https%3A%2F%2Fartworks.thetvdb.com%2Fbanners%2Fv4%2Fmovie%2F342389%2Fposters%2F65bc37cc4b0a0_t.jpg&w=384&q=75",
+    titre: "Red One",
+    duree: "2h03",
+    annee: "2024",
+  },
+  {
+    affiche:
+      "https://www.tvtime.com/_next/image?url=https%3A%2F%2Fartworks.thetvdb.com%2Fbanners%2Fv4%2Fmovie%2F167725%2Fposters%2F6490ab9ded239_t.jpg&w=384&q=75",
+    titre: "Kraven The Hunter",
+    duree: "2h07",
+    annee: "2024",
+  },
+  {
+    affiche:
+      "https://www.tvtime.com/_next/image?url=https%3A%2F%2Fartworks.thetvdb.com%2Fbanners%2Fv4%2Fmovie%2F330830%2Fposters%2F6612ef0f3b9d6_t.jpg&w=384&q=75",
+    titre: "Joker: Folie à Deux",
+    duree: "2h28",
+    annee: "2024",
+  },
+];
+
+export default function FilmCard() {
+  return (
+    <>
+      <div className="film-card">
+        <img src={filmsSelection.affiche} alt={filmsSelection.titre} />
+        <h2>{filmsSelection.titre}</h2>
+        <p>
+          {filmsSelection.duree} . {filmsSelection.annee}
+        </p>
+      </div>
+    </>
+  );
+}
