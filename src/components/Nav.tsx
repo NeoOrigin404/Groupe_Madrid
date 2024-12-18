2d36b8af910df58f7cd25a7a6793d4253ed975f3
Julien PICART <julienpicart01@gmail.com>
2024-12-18 08:58:22 +0100

    Navbar - OK

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -3,26 +3,33 @@ import "../styles/nav.css";
 export default function Nav() {
   return (
     <nav>
-      <div>
+      <div className="logo">
+        <img
+          src="https://play-lh.googleusercontent.com/wWoF0UMsnbJfBs2GahiNr3vpJjxAzfzc_GbgZrQWjNGRFFOY4z1Ky2-J0I1km-m1HQ"
+          alt="TV Time logo"
+        />
         <h1>MAD MADRID</h1>
       </div>
 
       <div id="tri">
         <p>Séries</p>
-
         <p>Films</p>
-
         <p>Genres</p>
       </div>
 
       <div id="search">
-        <input placeholder="Rechercher un film ou une série..." type="text" />
-        <button type="button">Rechercher</button>
+        <img src="./public/search.png" alt="" />
+        <input placeholder="Rechercher des titres" type="text" />
+        <select /* onChange={} */>
+          <option value="">All</option>
+          <option value="Film">Film</option>
+          <option value="Série">Série</option>
+        </select>
       </div>
 
       <div id="compte">
         <p>Se connecter</p>
-
+        <p>|</p>
         <p>S'inscrire</p>
       </div>
     </nav>
--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import "../styles/footer.css";
 export default function Footer() {
   return (
     <footer>
-      <h2>© 2024, TV Time, a Whip Media Company</h2>
+      <h2>© 2024, MAD MADRID, a Wild Wilder Company</h2>
     </footer>
   );
 }
